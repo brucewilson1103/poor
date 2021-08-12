67f3bf9bb5f0b29347e6552f845f32fd2e4bce19
da Vinci <brucewilson1103@gmail.com>
2021-08-12 14:28:13 -0400

    stateless widget for hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home:Scaffold(
+  runApp(TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
           title: Text('Knowledge Quest'),
           backgroundColor: Colors.teal,
         ),
-          backgroundColor: Colors.teal[200],
-          body: Center(
-            child: Image(
-              image: AssetImage('images/Owl.png'),
-              // image: NetworkImage('https://images.unsplash.com/photo-1593672755342-741a7f868732?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8MTAwJTIwZG9sbGFyJTIwYmlsbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
-            ),
+        backgroundColor: Colors.teal[200],
+        body: Center(
+          child: Image(
+            image: AssetImage('images/Owl.png'),
+            // image: NetworkImage('https://images.unsplash.com/photo-1593672755342-741a7f868732?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8MTAwJTIwZG9sbGFyJTIwYmlsbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
           ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
