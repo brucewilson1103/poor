5fb955ddeb5b7d5a082c6add01c1e00cbe90ff50
da Vinci <brucewilson1103@gmail.com>
2021-08-19 10:55:34 -0400

    formatting

--- lib/main.dart
+++ lib/main.dart
@@ -16,63 +16,69 @@ class TestApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('AppStore Business Card'),
+          title: Text('Contact Me'),
           backgroundColor: Colors.teal,
         ),
         backgroundColor: Colors.teal[200],
-        body: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage('images/bruce.png'),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.teal[200],
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundImage: AssetImage('images/bruce.png'),
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                'Bruce Wilson',
-                style: TextStyle(
-                  fontFamily: 'MonteCarlo',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40.0,
-                  color: Colors.white,
+                Text(
+                  'Bruce Wilson',
+                  style: TextStyle(
+                    fontFamily: 'MonteCarlo',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              Text(
-                'Flutter Developer',
-                style: TextStyle(
-                    fontFamily: 'EB Garamond',
-                    color: Colors.white70,
-                    fontSize: 25.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold),
-              ),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                margin:EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('BruceWilson1103@gmail.com')
-                  ],
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                      fontFamily: 'EB Garamond',
+                      color: Colors.white70,
+                      fontSize: 25.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-              // CircleAvatar(com
-              //   radius: 50,
-              //   backgroundImage: AssetImage('images/Owl.png'),
-              // ),
-            ],
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade400,
+                  ),
+                ),
+                Card(
+                  margin:EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(2.0),
+                    child:ListTile(
+                      leading:  Icon(
+                        Icons.email,
+                      ),
+                       title:  Text('BruceWilson1103@gmail.com'),
+                    )
+                  ),
+                ),
+                // CircleAvatar(com
+                //   radius: 50,
+                //   backgroundImage: AssetImage('images/Owl.png'),
+                // ),
+              ],
+            ),
+            // child:
           ),
-          // child:
         ),
       ),
     );
